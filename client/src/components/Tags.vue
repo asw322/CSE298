96b85f393d015a428797211f53ea766b2067ded5
Kenny Lin <kennylin119>
2021-01-03 15:36:08 -0500

    added animation using animate.css cdn

---
+++ client/src/components/Tags.vue
@@ -0,0 +1,22 @@
+// not working stuff copied from posts.vue 
+
+<template>
+  
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import PostsDataService from '../services/PostsDataService';
+
+
+@Component
+export default class Tags extends Vue {
+  private posts: Post[] = [];
+}
+</script>
+
+<style scoped>
+textarea.form-control {
+  height: 100px;
+}
+</style>
