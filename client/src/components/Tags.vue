ca35bb2e36418031e88b6dd8a7c08b451b4033d6
Kenny Lin <kennylin119>
2021-01-03 21:59:31 -0500

    added example table code onto tags.vue

--- client/src/components/Tags.vue
+++ client/src/components/Tags.vue
@@ -1,18 +1,26 @@
-// not working stuff copied from posts.vue 
-
 <template>
-  
+  <v-selectmenu :data="list" class="form-control" v-model="value">
+  </v-selectmenu>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import PostsDataService from '../services/PostsDataService';
-
+import Vue from 'vue'
+import vSelectMenu from 'v-selectmenu';
+Vue.use(vSelectMenu, { global config options... });
 
 @Component
 export default class Tags extends Vue {
-  private posts: Post[] = [];
-}
+   data(){
+      return {
+          value: '',
+          list: [
+            {id:1 ,name:'Chicago Bulls',desc:'芝加哥公牛'},
+            {id:2 ,name:'Cleveland Cavaliers',desc:'克里夫兰骑士'},
+            {...}
+          ]
+      };
+  };  
+};
 </script>
 
 <style scoped>
