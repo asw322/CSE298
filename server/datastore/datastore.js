26cd8f15c90c7654377217382d3ef0c45179aa87
Ralph Haddad <ralphhaddad777@gmail.com>
2021-01-03 20:56:37 -0800

    using client to enable queries
    how do we now parse the connection to allow for the pool method

--- server/datastore/datastore.js
+++ server/datastore/datastore.js
@@ -1,37 +1,50 @@
 //node-pg interface connection
 
 const { Pool } = require('pg')
+const { Client } = require('pg')
 
 require('dotenv').config()
 
 console.log('[postgres] ' + process.env.NODE_ENV + 'postgres config loaded')
 
-var config = {}
-if (process.env.NODE_ENV == 'development') {  //regular env
-  config = {
-    host: process.env.PSQL_HOSTNAME,
-    user: process.env.PSQL_USER,
-    password: process.env.PSQL_PASSWORD,
-    port: process.env.PSQL_PORT,
-    database: process.env.PSQL_DBNAME,
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
   }
-}
-else if (process.env.NODE_ENV == 'test') {  //use test environment (No test database currently)
-  console.log('testing server currently unimplimented, using main db')
-  config = {
-    host: process.env.PSQL_HOSTNAME,
-    user: process.env.PSQL_USER,
-    password: process.env.PSQL_PASSWORD,
-    port: process.env.PSQL_PORT,
-    database: process.env.PSQL_DBNAME,
+});
+
+// var config = {}
+// if (process.env.NODE_ENV == 'development') {  //regular env
+  
+// }
+// else if (process.env.NODE_ENV == 'test') {  //use test environment (No test database currently)
+//   console.log('testing server currently unimplimented, using main db')
+//   const client = new Client({
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: {
+//       rejectUnauthorized: false
+//     }
+//   });
+// }
+
+client.connect();
+
+
+
+client.query('SELECT * FROM employees', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
   }
-}
+  client.end();
+});
 
-const pool = new Pool(config);
+// const pool = new Pool(config);
 
-pool.on('connect', () => {
-  console.log('[postgres] Sucessful connection to postgres instance at [' + config.host + ']');
-})
+// pool.on('connect', () => {
+//   console.log('[postgres] Sucessful connection to postgres instance at [' + config.host + ']');
+// })
 
 const query = (text, params) => pool.query(text, params).catch(err => {throw err})
 
