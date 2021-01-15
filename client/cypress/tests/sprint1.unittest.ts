1336fa4156384e074c575c9bdd81c0d17bdb5eff
Kenny Lin <kennylin119>
2021-01-15 11:29:21 -0500

    commented out old broken unit test

--- client/cypress/tests/sprint1.unittest.ts
+++ client/cypress/tests/sprint1.unittest.ts
@@ -8,21 +8,21 @@ describe('Sprint 1', () => {
 
 //Kenny 
 // Tests to make sure voting is correct
-describe('Voting System', () => {
-    it('Should have an upvote button, a downvote button, and a vote count', () => {
-        // Check the upvote button:
-        cy.get('em')
-            .should('have.class', 'glyphicon glyphicon-chevron-up');
+// describe('Voting System', () => {
+//     it('Should have an upvote button, a downvote button, and a vote count', () => {
+//         // Check the upvote button:
+//         cy.get('em')
+//             .should('have.class', 'glyphicon glyphicon-chevron-up');
         
-        // Check the downvote button:
-        cy.get('span')
-            .should('have.class', 'label label-primary');
+//         // Check the downvote button:
+//         cy.get('span')
+//             .should('have.class', 'label label-primary');
         
-        // Check the vote count:
-        cy.get('em')
-            .should('have.class', 'glyphicon glyphicon-chevron-down');
-    });
-});
+//         // Check the vote count:
+//         cy.get('em')
+//             .should('have.class', 'glyphicon glyphicon-chevron-down');
+//     });
+// });
 
 // Manasi 
 describe('simple header', () => {
