cc6d92ae30a1a000ae8519acfba3595c78a368d0
Alan Wang <alanwang53@gmail.com>
2021-01-24 20:47:01 -0800

    modified index page

--- client/src/pages/IndexPage.vue
+++ client/src/pages/IndexPage.vue
@@ -13,9 +13,9 @@
         Sprint Scheduele
       </a>
       <router-link class="list-group-item list-group-item-action" to="/sprint0">Sprint 0 [DONE]</router-link>
-      <router-link class="list-group-item list-group-item-action" to="/sprint1">Sprint 1</router-link>
-      <router-link class="list-group-item list-group-item-action" to="/sprint2">Sprint 2</router-link>
-      <router-link class="list-group-item list-group-item-action" to="/sprint3">Sprint 3</router-link>
+      <router-link class="list-group-item list-group-item-action" to="/sprint1">Sprint 1 [DONE]</router-link>
+      <router-link class="list-group-item list-group-item-action" to="/sprint2">Sprint 2 [DONE]</router-link>
+      <router-link class="list-group-item list-group-item-action" to="/sprint3">Sprint 3 [DONE]</router-link>
     </div>
   </div>
 </template>
